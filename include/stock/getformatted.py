7592faeb2bcc1b7fda1bf517701401c7cf3dd5b5
Mohammed Taha <motaha295d@gmail.com>
2026-06-03 13:27:08 +0300

    refactor: optimize reading & writing using S3Hook

--- include/stock/getformatted.py
+++ include/stock/getformatted.py
@@ -1,5 +1,5 @@
-from include.helpers.minio import get_minio_client
 from airflow.exceptions import AirflowNotFoundException
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 def get_formatted_csv(path):
     '''
@@ -7,22 +7,19 @@ def get_formatted_csv(path):
     Args:
         path (str): The full path where the formatted data is stored in MinIO.
     Returns:
-        csv: The formatted stock data as a CSV.
+        str: The same path if the file exists.
     '''
 
     # path value e.g.-> stock-data/NVDA/prices_formatted.csv
 
-    client = get_minio_client()
-    bucket_name = path.split('/')[0]  # Extract bucket name from the path
-    object_name = '/'.join(path.split('/')[1:])  # Extract object name from the path
+    s3_hook = S3Hook(aws_conn_id='minio_conn')
+    # bucket_name = path.split('/')[0]  # Extract bucket name from the path
+    # key = '/'.join(path.split('/')[1:])  # Extract object name from the path
+    bucket_name, key = path.split("/", 1)
 
-    try:
-        client.stat_object(bucket_name, object_name)
-        return path  # ← just the path, no data!    except Exception as e:
-    except Exception as e:
-
+    if not s3_hook.check_for_key(key=key, bucket_name=bucket_name):
         raise AirflowNotFoundException(
-            f"CSV file not found at {path}: {e}"
+            f"CSV file not found at {path}"
         )
 
-
+    return path 
