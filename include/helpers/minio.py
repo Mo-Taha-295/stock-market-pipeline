0a12f84b2e23014a5b5b4ede4f305e930e01bcd3
Mohammed Taha <motaha295d@gmail.com>
2026-06-05 15:04:13 +0300

    refactor(helpers): replace custom MinIO client with S3Hook
    Commented out the manual get_minio_client connection handling helper.
    Migrated to Airflow's native  to manage S3/MinIO connections
    using standard Airflow connection IDs.

--- include/helpers/minio.py
+++ include/helpers/minio.py
@@ -1,12 +1,12 @@
-from minio import Minio
-from airflow.hooks.base import BaseHook
+# from minio import Minio
+# from airflow.hooks.base import BaseHook
 
-def get_minio_client():
-    minio_conn = BaseHook.get_connection("minio_conn") 
-    client = Minio(
-        endpoint=minio_conn.extra_dejson.get('endpoint_url').split('//')[1],  # Extract host:port from the URL
-        access_key=minio_conn.login,
-        secret_key=minio_conn.password,
-        secure=False  # Set to True if using HTTPS
-    )
-    return client
+# def get_minio_client():
+#     minio_conn = BaseHook.get_connection("minio_conn") 
+#     client = Minio(
+#         endpoint=minio_conn.extra_dejson.get('endpoint_url').split('//')[1],  # Extract host:port from the URL
+#         access_key=minio_conn.login,
+#         secret_key=minio_conn.password,
+#         secure=False  # Set to True if using HTTPS
+#     )
+#     return client
